92114f1815fc8b37cc083213ae7699fcfa914817
katerina <klarina2409@gmail.com>
2024-05-27 11:07:22 +0300

    2

---
+++ src/main/java/Transactions/bank/Transactions/Role.java
@@ -0,0 +1,6 @@
+package Transactions.bank.Transactions;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/Transactions/bank/Transactions/repository/UserRepository.java
+++ src/main/java/Transactions/bank/Transactions/repository/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
     boolean existsByLogin(String login);
@@ -12,4 +14,8 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
     boolean existsByEmail(String email);
 
     boolean existsByPhone(String phone);
+
+//    Optional<User> findByUsername(String username);
+//    boolean existsByUsername(String username);
+//    boolean existsByEmail(String email);
 }
